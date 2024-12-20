a62fbe4f5c6a728bc4a8d9f2e249c0f362e6af74
g6g6g8p8 <142065757+g6g6g8p8@users.noreply.github.com>
2024-12-20 10:31:17 -0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="mt-auto py-6 px-4 border-t border-gray-200 dark:border-gray-800">
+      <div className="max-w-site mx-auto text-center text-gray-600 dark:text-gray-400 text-sm">
+        © Creative Whatever Since 1988
+      </div>
+    </footer>
+  );
+}
